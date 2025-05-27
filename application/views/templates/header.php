51cb60da0202229f7e59b480e4b870c865e1ece9
teripangijo <teripang.ijo@gmail.com>
2025-05-27 11:47:11 +0700

    Add icon

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -24,6 +24,9 @@
 
     <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
 
+    <link rel="icon" href="<?= base_url('favicon.ico'); ?>" type="image/x-icon">
+    <link rel="shortcut icon" href="<?= base_url('favicon.png'); ?>" type="image/x-icon">
+
 </head>
 
 <body id="page-top">
