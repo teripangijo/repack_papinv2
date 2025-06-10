25d76bba72b890a73e2f031d61e92b3ab12721a1
teripangijo <teripang.ijo@gmail.com>
2025-06-10 14:26:23 +0700

    Changing image format

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -33,7 +33,7 @@
                                 <div class="col-lg-12">
                                     <div class="p-4 p-sm-5">
                                         <div class="text-center">
-                                            <img src="<?= base_url('/assets/img/logo_papin.png');?>" alt="Logo Instansi" class="login-logo mb-3">
+                                            <img src="<?= base_url('/assets/img/logo_papin.webp');?>" alt="Logo Instansi" class="login-logo mb-3">
                                             <h1 class="h4 text-gray-900 mb-2">Selamat Datang</h1>
                                             <p class="text-muted mb-4">Login ke akun REPACK Anda.</p>
                                         </div>
